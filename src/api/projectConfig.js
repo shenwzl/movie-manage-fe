d2914b28143eb707d264cb437eab6e7e8ae041b1
沈智 <zhishen_1@tujia.com>
2019-07-23 19:53:07 +0800

    change path

--- src/api/projectConfig.js
+++ src/api/projectConfig.js
@@ -18,7 +18,7 @@ export function getContracts(params) {
 
 export function getCompanys(params) {
   return request({
-    url: '/configs/companys',
+    url: '/configs/customer_companies',
     method: 'get',
     params
   })
